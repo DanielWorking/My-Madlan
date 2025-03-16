e6d6657a8083c0e890819938be6b357503b8e01a
DanielWorking <134011193+DanielWorking@users.noreply.github.com>
2025-03-16 08:49:01 +0200

    first

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RegisterPage = () => {
+    return (
+        <>
+            <div className="register">
+                <div className="register_content"></div>
+            </div>
+        </>
+    );
+};
+
+export default RegisterPage;
